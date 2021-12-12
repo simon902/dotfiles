96ff99dff228407ec6327043c4a716ac065e180f
Simon Scherer <simon.scherer@student.tugraz.at>
2021-12-12 13:54:21 +0100

    basic installation script.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+curr_path=$(pwd)
+
+ln -s $curr_path/.zshenv $HOME/.zshenv
+
+mkdir -p $HOME/.config/zsh
+
+ln -s $curr_path/.config/zsh/.zcompdump $HOME/.config/zsh/.zcompdump
+ln -s $curr_path/.config/zsh/.zprofile $HOME/.config/zsh/.zprofile
+ln -s $curr_path/.config/zsh/.zshrc $HOME/.config/zsh/.zshrc
+
