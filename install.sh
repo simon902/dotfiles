679c7b948a1d4a1d098a85d54e1848c180779204
Simon Scherer <simon.scherer@student.tugraz.at>
2022-01-02 10:48:30 +0100

    Update Install script

--- install.sh
+++ install.sh
@@ -9,4 +9,5 @@ mkdir -p $HOME/.config/zsh
 ln -s $curr_path/.config/zsh/.zcompdump $HOME/.config/zsh/.zcompdump
 ln -s $curr_path/.config/zsh/.zprofile $HOME/.config/zsh/.zprofile
 ln -s $curr_path/.config/zsh/.zshrc $HOME/.config/zsh/.zshrc
+ln -s $curr_path/.config/starship.toml $HOME/.config/starship.toml
 
