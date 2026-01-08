3977d3d705bec722ea19546f4ea474be19929ae6
Simon Scherer <simon.scherer@student.tugraz.at>
2026-01-08 17:46:26 +0100

    Fix: zsh create missing scripts folder

--- install/dependencies/pre/zsh.sh
+++ install/dependencies/pre/zsh.sh
@@ -3,6 +3,7 @@
 CONFIG_ROOT=$1
 
 touch "$CONFIG_ROOT/.config/zsh/aliases" # if file already existed touch doesn't override
+mkdir -p "$CONFIG_ROOT/.config/zsh/scripts/"
 cd "$CONFIG_ROOT/.config/zsh/scripts/"
 
 [ ! -d "fzf-tab" ] && git clone https://github.com/Aloxaf/fzf-tab
